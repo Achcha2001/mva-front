e6ce3221b2225b6123ab08e0e99830e4e8d07500
Achcha2001 <113570864+Achcha2001@users.noreply.github.com>
2025-04-12 18:53:12 +0530

    circular update 02.1

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import './gallery.css'; // Link to your CSS
 
 function Gallery() {
   return (
-    <Container>
-      <Row>
+    <Container fluid className="gallery-container">
+      <div className="under-construction-banner">
+        🚧 This page is under construction. Please check back soon! 🚧
+      </div>
+      <Row className="mt-5">
         <Col>
-          <h1>Gallery</h1>
-          {/* Add Image Gallery */}
+          <h1 className="gallery-heading">Gallery</h1>
+          {/* Future Image Gallery goes here */}
         </Col>
       </Row>
     </Container>
--- src/components/Ranks.js
+++ src/components/Ranks.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import './TeamRanking.css'; // Make sure to create this CSS file
 
 function TeamRanking() {
-  return <h1>Team Ranking Page</h1>;
+  return (
+    <div className="team-ranking-container">
+      <div className="under-construction-banner">
+        🚧 This page is under construction. Please check back soon! 🚧
+      </div>
+      <h1 className="team-ranking-heading">Team Ranking Page</h1>
+    </div>
+  );
 }
 
 export default TeamRanking;
